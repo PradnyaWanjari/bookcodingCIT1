dba232323f1d7a2e033ef85fc1af847ea020941d
PradnyaWanjari <pradnyaw201103@gmail.com>
2023-08-19 12:19:58 +0530

    java

---
+++ src/java/Condition.java/Array.java
@@ -0,0 +1,8 @@
+package src.java.Condition.java;
+
+public class Array {
+    public static void main(String[] args) {
+        String name[] = {"chayank","yugal","krishna","deshyank"};
+        System.out.println(name[2]);
+    }
+}
---
+++ src/java/Condition.java/Hello.java
@@ -0,0 +1,8 @@
+package src.java.Condition.java;
+
+public class Hello {
+    public static void main(String[] arg) {
+        System.out.print("src.java.Condition.java.Hello world");
+    }
+
+}
